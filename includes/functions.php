d1985b23d36cab3c587ab1f1d7cd3b883ff69543
bikashydv <85330134+bikashydv@users.noreply.github.com>
2021-09-27 19:00:09 +0545

    Add files via upload

--- includes/functions.php
+++ includes/functions.php
@@ -8,6 +8,19 @@ function getCategory($db, $id)
     return $data['name'];
 }
 
+
+function getAllCategory($db)
+{
+    $query = "SELECT * FROM category ";
+    $run = mysqli_query($db, $query);
+    $data = array();
+    while ($d = mysqli_fetch_assoc($run)) {
+        $data[] = $d;
+    }
+    return $data;
+}
+
+
 function getImagesByPost($db, $post_id)
 {
     $query = "SELECT * FROM images WHERE post_id=$post_id ";
